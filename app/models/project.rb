cfe6de92603f2f2f1b639a9856badc064906c1cb
Ludovic Villain <ludovic@MacBook-Pro-de-Ludovic.local>
2021-10-04 10:39:34 +0200

    Seeds done

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :categories
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,13 @@
 
 puts "start seeds"
 
+Project.destroy_all
+Category.destroy_all
+User.destroy_all
+
 # USER
 puts "creation user start"
-administrateur = User.create!(email: "chadludo@gmail.com", password: "chad$195")
+ludovic = User.create!(email: "chadludo@gmail.com", password: "chad$195")
 
 # CATEGORIES
 puts "creation category start"
@@ -21,6 +25,6 @@ elearning = Category.create!(name: "E-learning")
 
 # PROJECTS
 puts "creation projects start"
-beaumanoir = Project.create!(name: "Beaumanoir", user: "chadludo@gmail.com", description: "Conception d’un site e-learning pour former les équipes du groupe Beaumanoir (Cache Cache, Morgan…) à la charte éthique de l’entreprise.", date:"2019", role:"Mes objectifs sur le projet Charte Ethique étaient de traduire visuellement des sujets importants pour le groupe de manière ludique et moderne. L’interaction et le mouvement ont donc été au cœur de mes réflexions pour sa conception. Les éléments de navigation se veulent discrets et élégants afin de ne pas surcharger l’interface. L’utilisateur peut ainsi pleinement se concentrer sur le contenu pédagogique.")
+beaumanoir = Project.create!(name: "Beaumanoir", user: ludovic, category: elearning, description: "Conception d’un site e-learning pour former les équipes du groupe Beaumanoir (Cache Cache, Morgan…) à la charte éthique de l’entreprise.", date:"2019", role:"Mes objectifs sur le projet Charte Ethique étaient de traduire visuellement des sujets importants pour le groupe de manière ludique et moderne. L’interaction et le mouvement ont donc été au cœur de mes réflexions pour sa conception. Les éléments de navigation se veulent discrets et élégants afin de ne pas surcharger l’interface. L’utilisateur peut ainsi pleinement se concentrer sur le contenu pédagogique.")
 
 puts "seeds done!"
